278a3d720c97b581e04d75e53de272f44303eda2
jerametabanao55-commits <jerametabanao55@gmail.com>
2025-12-11 08:56:24 +0800

    Create GreetingProgram to greet user by name and age

---
+++ GreetingProgram .java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GreetingProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Hello " + name + "! You are " + age + " years old.");
+        sc.close();
+    }
+}
